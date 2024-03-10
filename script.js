fe1b39299389ea519ba454f8f647217708675c88
Pritpal Singh <90276050+pritpalcodes@users.noreply.github.com>
2024-03-10 21:10:58 +0530

    modern responsive website using gsap

---
+++ script.js
@@ -0,0 +1,121 @@
+var timeout;
+
+//inverting the button
+document.getElementById("lets_talk_button").addEventListener("mouseover", function() {
+  this.style.backgroundColor = "white";
+  this.style.color = "black";
+});
+
+
+document.getElementById("lets_talk_button").addEventListener("mouseleave", function() {
+  this.style.backgroundColor = "black";
+  this.style.color = "white";
+});
+
+
+
+//loco scroll
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+});
+
+
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 1.5,
+      delay: -1,
+      stagger: 0.2,
+    })
+    .from("#herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 2,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+function circleChaptaKaro() {
+  // define default scale value
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    xscale = gsap.utils.clamp(0.4, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.4, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
+
+
+
+function circleMouseFollower() {
+    window.addEventListener("mousemove", function(dets) {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    });
+}
+
+circleMouseFollower(); // Call the function to start following the mouse cursor
+
+
+firstPageAnim();
+circleChaptaKaro();
+circleMouseFollower();
+
+
+
+
+//teeno element ko select karo, teeno pr mousemove select kro, mouse move pta kro kahan h, (x,y) posn, and x,y ki jagah image show karo and, image ko rotate bhi karna h, with the mouse, also, jaise jaise mouse move fast ho, waise hi image rotation bhi fast ho jaye!
+
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+  var rotate = 0;
+  var diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
